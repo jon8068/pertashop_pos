585e08e1bc5b4ce123cc1ad9fe0992dc579ddf64
Ketziaseraphim <chiaaarella@gmail.com>
2022-09-19 21:44:37 +0700

    first commit

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  getUsers,
+  getUserById,
+  saveUser,
+} from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", saveUser);
+
+export default router;
